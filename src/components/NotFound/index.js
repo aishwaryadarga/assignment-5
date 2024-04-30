8b1d0c8f230ba94babab235b0666ab8ed3387c4e
aishwaryadarga <aishwaryadarga19@gmail.com>
2024-04-30 05:14:30 +0000

    code giving error

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,8 @@
+// Write your code here
+import './index.css'
+
+const NotFound = () => (
+   <div className="not-found-route-container">
+      <h1 className="not-found-heading">Page Not found !!</h1> 
+   </div>
+)
